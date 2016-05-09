8c0e77876bd6c0b7041a5d9f007ce5b520dccc10
Georges Kfoury <ggk07@mail.aub.edu>
2016-05-09 08:30:48 +0300

    gg

--- app/src/main/java/com/emadyehya/eece451/MainActivity.java
+++ app/src/main/java/com/emadyehya/eece451/MainActivity.java
@@ -1,23 +1,36 @@
 package com.emadyehya.eece451;
 
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     //region GLOBAL_VARIABLES
     Context context;
+    EditText editTextDialogUserInput;
+    String url = "http://emadyehya.com/add_nb";
+    int phone_number;
+    String response;
     //endregion
 
     @Override
@@ -55,6 +68,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(context, GraphActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // custom dialog
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.prompts);
+                //dialog.setTitle("Title...");
+                editTextDialogUserInput = (EditText) dialog.findViewById(R.id.editTextDialogUserInput);
+
+                Button dialogButton = (Button) dialog.findViewById(R.id.button_ok);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            phone_number = Integer.parseInt(editTextDialogUserInput.getText().toString());
+                            new SendPhoneNumber().execute();
+                        }
+                        catch(Exception E) {
+                            dialog.dismiss();
+                            final Dialog newDialog = new Dialog(context);
+                            newDialog.setContentView(R.layout.custom);
+                            Button button = (Button) newDialog.findViewById(R.id.button_ok);
+                            newDialog.show();
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    newDialog.dismiss();
+                                }
+                            });
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
             }
         });
 
@@ -114,5 +167,32 @@ public class MainActivity extends AppCompatActivity {
         ((Switch) findViewById(R.id.main_switch_bluetooth)).setChecked(bluetooth_status);
         ((Switch) findViewById(R.id.main_switch_wifi)).setChecked(wifi_status);
         ((Switch) findViewById(R.id.main_swtich_cell)).setChecked(cellular_status);
+    }
+
+    private class SendPhoneNumber extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+            // Making a request to url and getting response
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add (new BasicNameValuePair("nb", "" + phone_number ));
+            response = sh.makeServiceCall(url,ServiceHandler.POST,params);
+            return response;
+
+        }
+        protected void onPostExecute(String result) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
